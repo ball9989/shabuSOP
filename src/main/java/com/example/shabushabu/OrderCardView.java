b0e1f05af209e2310bb69bd36fdbb54af1686aa6
ball9988 <krisakorn9988@gmail.com>
2021-12-15 01:26:04 +0700

    Order card

--- src/main/java/com/example/shabushabu/OrderCardView.java
+++ src/main/java/com/example/shabushabu/OrderCardView.java
@@ -1,13 +1,32 @@
 package com.example.shabushabu;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-public class OrderCardView extends VerticalLayout {
+
+@StyleSheet("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
+public class OrderCardView extends Div {
     Image image = new Image("https://dummyimage.com/600x400/000/fff", "DummyImage");
+    Div body = new Div();
+    H5 title = new H5("ชื่ออาหาร:");
+    H6 subtitle = new H6("รายละเอียด:");
+    Paragraph para = new Paragraph("ราคา:");
+
+
     public OrderCardView(){
+        this.addClassName("card");
+        this.image.addClassName("card-img-top");
+        this.body.addClassName("card-body");
+        this.title.addClassName("card-title");
+        this.subtitle.addClassName("card-subtitle");
+        this.para.addClassName("card-text");
+
+        body.add(title,subtitle,para);
         this.add(image);
-        this.add(new H1("test"));
+        this.add(body);
+
 
 
     }
