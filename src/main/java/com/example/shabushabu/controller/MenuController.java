62b3cd06c734274a63290d69a0c0a2faf877b50c
Yannawut62070051 <56510625+Yannawut62070051@users.noreply.github.com>
2021-12-17 04:18:06 +0700

    CreateAndDelete

--- src/main/java/com/example/shabushabu/controller/MenuController.java
+++ src/main/java/com/example/shabushabu/controller/MenuController.java
@@ -2,14 +2,19 @@ package com.example.shabushabu.controller;
 
 import com.example.shabushabu.pojo.Menu;
 import com.example.shabushabu.pojo.Menus;
+import com.example.shabushabu.pojo.Order;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @RestController
 public class MenuController {
@@ -25,4 +30,26 @@ public class MenuController {
 
         return ResponseEntity.ok(menu);
     };
+
+    @RequestMapping(value = "/addMenu", method = RequestMethod.POST)
+    public boolean addMenu(@RequestBody MultiValueMap<String, String> menu) {
+        Map<String, String> d = menu.toSingleValueMap();
+        String name = d.get("name");
+        Integer mats_left = Integer.parseInt(d.get("mats_left"));
+        Integer mats_cost = Integer.parseInt(d.get("mats_cost"));
+        String detail = d.get("detail");
+        String image = d.get("image");
+        Integer price = Integer.parseInt(d.get("price"));
+        System.out.println(name+mats_left+mats_cost+image+price);
+        rabbitTemplate.convertAndSend("ShabuOrder", "addMenu", new Menu(null,name,image,detail,price,mats_left,mats_cost));
+        return true;
+    }
+
+    @RequestMapping(value = "/deleteMenu", method = RequestMethod.POST)
+    public boolean deleteMenu(@RequestBody MultiValueMap<String, String> menu){
+        Map<String, String> d = menu.toSingleValueMap();
+        String _id = d.get("_id");
+        rabbitTemplate.convertAndSend("ShabuOrder","deleteMenu", _id);
+        return true;
+    }
 }
--- src/main/java/com/example/shabushabu/pojo/Account.java
+++ src/main/java/com/example/shabushabu/pojo/Account.java
@@ -27,4 +27,52 @@ public class Account implements Serializable {
         this.tableNo = tableNo;
         this.token = token;
     }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getTableNo() {
+        return tableNo;
+    }
+
+    public void setTableNo(int tableNo) {
+        this.tableNo = tableNo;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
