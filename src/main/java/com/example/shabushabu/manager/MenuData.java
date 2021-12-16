39fdd30a2725ab0f5e7bb9f84512d8149d896dc7
Yannawut62070051 <56510625+Yannawut62070051@users.noreply.github.com>
2021-12-16 21:07:35 +0700

    Update MenuData.java

--- src/main/java/com/example/shabushabu/manager/MenuData.java
+++ src/main/java/com/example/shabushabu/manager/MenuData.java
@@ -2,24 +2,99 @@ package com.example.shabushabu.manager;
 
 import com.example.shabushabu.pojo.Menu;
 import com.example.shabushabu.pojo.Menus;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.awt.*;
+
+@StyleSheet("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
 public class MenuData extends VerticalLayout {
     private Menus menu;
+    private H5 menuName,menuPrice,menuDetail;
+    private Button addMenu;
+    private H2 title = new H2("เมนูทั้งหมด");
     public MenuData(){
         getMenu();
-        H2 title = new H2("เมนู");
+        addMenu = new Button("เพิ่มเมนู",new Icon(VaadinIcon.PLUS));
         Grid<Menu> grid = new Grid<>(Menu.class, false);
-        grid.addColumn(Menu::getName).setHeader("ชื่ออาหาร");
-        grid.addColumn(Menu::getPrice).setHeader("ราคา (บาท)");
-        grid.addColumn(Menu::getDetail).setHeader("รายละเอียด");
+        grid.addComponentColumn(menu ->{
+            Button editButton = new Button("แก้ไข");
+            editButton.addClickListener(e->{
+                System.out.println(menu.getPrice());
+            });
+            return editButton;
+        }).setWidth("12%").setFlexGrow(0).setHeader(addMenu);
+        grid.addColumn(Menu::getName).setHeader("ชื่อเมนู").setFlexGrow(0);
+        grid.addColumn(Menu::getPrice).setHeader("ราคา(บาท)").setFlexGrow(0);
+        grid.addColumn(Menu::getDetail).setHeader("คำอธิบาย");
         grid.setItems(menu.model);
         add(title,grid);
-        setWidth("100%");
         setVisible(false);
+
+        Dialog dialog = new Dialog();
+        dialog.getElement().setAttribute("aria-label", "Add note");
+
+        VerticalLayout dialogLayout = createDialogLayout(dialog);
+        dialog.add(dialogLayout);
+        dialog.setModal(false);
+        dialog.setDraggable(true);
+
+        Button button = new Button("Show dialog", e -> dialog.open());
+        add(dialog, button);
+    }
+
+    private static VerticalLayout createDialogLayout(Dialog dialog) {
+        H2 headline = new H2("Add note");
+        headline.getStyle().set("margin", "0").set("font-size", "1.5em")
+                .set("font-weight", "bold");
+        HorizontalLayout header = new HorizontalLayout(headline);
+        header.getElement().getClassList().add("draggable");
+        header.setSpacing(false);
+        header.getStyle()
+                .set("border-bottom", "1px solid var(--lumo-contrast-20pct)")
+                .set("cursor", "move");
+        header.getStyle()
+                .set("padding", "var(--lumo-space-m) var(--lumo-space-l)")
+                .set("margin",
+                        "calc(var(--lumo-space-s) * -1) calc(var(--lumo-space-l) * -1) 0");
+
+        TextField titleField = new TextField("ชื่อเมนู");
+        TextArea descriptionArea = new TextArea("Description");
+        VerticalLayout fieldLayout = new VerticalLayout(titleField, descriptionArea);
+        fieldLayout.setSpacing(false);
+        fieldLayout.setPadding(false);
+        fieldLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+
+        Button cancelButton = new Button("ยกเลิก", e -> dialog.close());
+        Button saveButton = new Button("เพิ่มเมนู", e -> dialog.close());
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        HorizontalLayout buttonLayout = new HorizontalLayout(cancelButton,
+                saveButton);
+        buttonLayout
+                .setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+
+        VerticalLayout dialogLayout = new VerticalLayout(header, fieldLayout,
+                buttonLayout);
+        dialogLayout.setPadding(false);
+        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        dialogLayout.getStyle().set("width", "300px").set("max-width", "100%");
+
+        return dialogLayout;
     }
 
     public void getMenu(){
@@ -29,6 +104,12 @@ public class MenuData extends VerticalLayout {
                 .bodyToMono(Menus.class)
                 .block();
         menu = out;
-        System.out.println("getMenu"+out.model);
     }
+    public void test(){
+        System.out.println("Test");
+        remove(title);
+        title.setText("test");
+        add(title);
+    }
+
 }
